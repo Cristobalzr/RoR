6c91b509795cc880fcc1eb5c3bf1b44a7b529d6a
Cristobal <cristobalz@outlook.com>
2019-05-10 12:19:13 -0400

    commit2

--- desafios_clase_1_y_2/clase2/course.rb
+++ desafios_clase_1_y_2/clase2/course.rb
@@ -1,2 +1,31 @@
+require 'date'
+
 class Course
+  def initialize(name, *dates)
+    @name = name
+    @dates = dates.map{|date| Date.parse(date)}
+  end
+  def before_dates(filter_date)
+    if @dates[0] < filter_date
+      print @dates[0]
+    end
+  end
+  def after_dates(filter_date)
+    if @dates[0] > filter_date
+      print @dates[0]
+    end
+  end
 end
+
+file = File.open('cursos.txt', 'r')
+data = file.readlines
+file.close
+
+filter = []
+
+data.each do |d|
+  ls = d.split(', ')
+  filter << Course.new(*ls)
+end
+
+filter[0].after_dates(Date.today)
