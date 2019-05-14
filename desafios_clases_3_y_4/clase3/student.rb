79439c490d543541c62b7dde321ae3041a9f0238
Cristobal <cristobalz@outlook.com>
2019-05-14 09:28:52 -0400

    student listo

---
+++ desafios_clases_3_y_4/clase3/student.rb
@@ -0,0 +1,36 @@
+module Test
+  def result
+    if (@nota1 + @nota2)/2 >= 4
+      puts "Estdiante aprobado"
+    else
+      "Estuidiante reprobado"
+    end
+  end
+end
+
+module Attendance
+  def self.student_quantify
+    Student.student_quantify
+  end
+end
+
+class Student
+  include Test
+  extend Attendance
+  attr_reader :name, :grade1, :grade2, :quantify
+  @@quantify = 0
+
+  def initialize(nombre,nota1 = 4,nota2 = 4)
+    @nombre = nombre
+    @nota1 = nota1.to_f
+    @nota2 = nota2.to_f
+    @@quantify +=1
+  end
+
+  def self.student_quantify
+    @@quantify
+  end
+end
+
+10.times { Student.new("pepito").result}
+puts Attendance.student_quantify
